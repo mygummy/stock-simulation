f2995797831c83b88d2248b870c019c65fdffa98
hyunmin <lhm6199@naver.com>
2022-12-02 01:34:41 +0900

    change main.js

--- main.js
+++ main.js
@@ -2,26 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// let counter = 0;
-
-// app.get('/counter', (req, res) => {
-//     counter++;
-//     res.send(counter.toString())
-// })
-
-// app.use(express.json()) // for parsing application/json
-
-// let chats = [];
-
-// app.get('/chats', (req, res) => {
-//     res.send(chats);
-// })
-
-// app.post('/chats', (req, res) => {
-//     chats.push(req.body.chat);
-//     res.send(200);
-// })
-
 app.use(express.static('public'))
 
 app.listen(port, () => {
